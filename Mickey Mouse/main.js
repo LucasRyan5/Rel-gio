a537cd37d57570c1290cb32fc49bfb3cd648e627
Lucas Ryan <lr749373@gmail.com>
2024-03-26 20:53:07 -0300

    Add files via upload

--- Mickey Mouse/main.js
+++ Mickey Mouse/main.js
@@ -1,2 +1,18 @@
 //colocar o coódigo aqui...
- 
+const hoursElement = document.getElementById('hours') 
+const minutesElement = document.getElementById('minutes')
+const secondsElement = dcument.getElementById('seconds')
+
+const date = new date();
+
+const hours = date.getHours()
+const minutes = date.getMinutes()
+const seconds = date.getSeconds()
+
+hoursElement.textContent = fixTime (hours)
+minutesElement.textContent = fixTime (minutes)
+secondsElement.textContent = fixTime (seconds)
+
+function fixTime{
+    return time <10 ? '0' <+ time : time
+}
